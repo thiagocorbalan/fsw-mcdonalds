93e5bf49d9237e5738e55d0e43798786432b7408
Thiago Corbalan <contato.twebdesigner@gmail.com>
2025-02-19 23:59:59 -0300

    chore: update css variables

---
+++ src/app/banana/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Page() {
+    return (
+        <>
+            <Button>Banana</Button>
+            <Input />
+        </>
+    )
+}
